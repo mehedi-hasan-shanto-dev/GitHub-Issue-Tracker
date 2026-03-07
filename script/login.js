1c81d4efca68edb7c4f55df341e31d5d7da61278
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-03-07 21:51:58 +0600

    home.html header and search clear

---
+++ script/login.js
@@ -0,0 +1,14 @@
+const userName = document.getElementById("userName");
+const password = document.getElementById("password");
+
+// step-1: login Functionality
+const btnSubmit = () => {
+  const inputUserName = userName.value;
+  const inputPassword = password.value;
+
+  if (inputUserName === "admin" && inputPassword === "admin123") {
+    window.location.assign("/pages/home.html");
+  } else {
+    alert("Wrong Credentials");
+  }
+};
